3789ac38e2261f631c002d7f4febf5e1c0313c1f
Abhiram Shibu <abhiramshibu1998@gmail.com>
2021-06-23 00:18:50 +0530

    Core: Now TCP Server will start with application

--- src/core/Core.java
+++ src/core/Core.java
@@ -2,17 +2,21 @@ package core;
 import java.io.IOException;
 
 import configuration.ConfigInit;
+import configuration.Configuration;
+import connectivity.TCPServer;
 import logging.LogManager;
 
 public class Core {
 	public static LogManager logManager ;
     public static void main(String[] args) {
     	logManager = new LogManager();
+    	logManager.log(Core.class.getName(), "Welcome to AnonFS version: "+Configuration.version+" core now alive!");
     	try {
 			new ConfigInit();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+    	TCPServer tcpServer = new TCPServer(Configuration.DEFAULT_PORT);
     }
 }

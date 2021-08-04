6e6399a9d6a613dc538d695bc1e04073575738b5
Abhiram Shibu <abhiramshibu1998@gmail.com>
2021-08-05 00:53:52 +0530

    Core: Config Parser variable and Auto Connector added

--- src/core/Core.java
+++ src/core/Core.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 
 import configuration.ConfigInit;
 import configuration.Configuration;
+import connectivity.AutoConnector;
 import connectivity.TCPServer;
 import datastructures.PeerDataHandler;
 import logging.LogManager;
@@ -10,16 +11,19 @@ import logging.LogManager;
 public class Core {
 	public static LogManager logManager ;
 	public static PeerDataHandler pdh;
+	public static ConfigInit config;
     public static void main(String[] args) {
     	logManager = new LogManager();
     	logManager.log(Core.class.getName(), "Welcome to AnonFS version: "+Configuration.version+" core now alive!");
     	try {
-			new ConfigInit();
+			config = new ConfigInit();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
     	pdh = new PeerDataHandler();
+    	AutoConnector ac = new AutoConnector();
+    	ac.start();
     	TCPServer tcpServer = new TCPServer(Configuration.DEFAULT_PORT);
     }
 }

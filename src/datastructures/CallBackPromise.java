8a7af610608272a041dbb896fc2ca177a5f2dc3c
Abhiram Shibu <abhiramshibu1998@gmail.com>
2021-08-28 01:42:06 +0530

    CallBackPromise: Added callback promise..

---
+++ src/datastructures/CallBackPromise.java
@@ -0,0 +1,10 @@
+package datastructures;
+
+public class CallBackPromise {
+	public int refid;
+	public byte[] data;
+	public CallBackPromise(int refid) {
+		// TODO Auto-generated constructor stub
+		this.refid = refid;
+	}
+}

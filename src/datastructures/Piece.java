239e995b39777507b372204a4ccb565679c3e348
Abhiram Shibu <abhiramshibu1998@gmail.com>
2021-10-09 17:18:19 +0530

    Piece: Partially completed

--- src/datastructures/Piece.java
+++ src/datastructures/Piece.java
@@ -1,5 +1,13 @@
 package datastructures;
 
+import java.util.LinkedList;
+
+class Header{
+	LinkedList<Object> metadata;
+	public Header() {
+		
+	}
+}
 public class Piece {
 	public Piece() {
 		byte body[];

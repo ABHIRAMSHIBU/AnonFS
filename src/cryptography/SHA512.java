0aca7e07eb2b070d195fd7e96e4a2689d44f35e1
Abhiram Shibu <abhiramshibu1998@gmail.com>
2021-10-17 16:07:25 +0530

    SHA512: Checksum class added

---
+++ src/cryptography/SHA512.java
@@ -0,0 +1,27 @@
+package cryptography;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SHA512 {
+	public static byte[] digest(byte [] in) throws NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance("SHA-512");
+		byte[] messageDigest = md.digest(in);
+		return in;
+	}
+	public static boolean verify(byte [] in, byte [] checksum) throws NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance("SHA-512");
+		byte[] messageDigest = md.digest(in);
+		if(messageDigest.length != checksum.length) {
+			return false;
+		}
+		else {
+			for(int i=0; i<messageDigest.length; i++) {
+				if(messageDigest[i] != checksum[i]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+}

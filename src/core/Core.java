719107d8d88d3c04d01d7dabf70d2a7cc9bd0cdd
Abhiram Shibu <abhiramshibu1998@gmail.com>
2021-10-22 23:04:13 +0530

    Core: Added peer discovery

--- src/core/Core.java
+++ src/core/Core.java
@@ -6,12 +6,14 @@ import configuration.ConfigInit;
 import configuration.Configuration;
 import connectivity.AutoConnector;
 import connectivity.ConnectionIDHander;
+import connectivity.PeerDiscovery;
 import connectivity.TCPServer;
 import datastructures.PeerDataHandler;
 import logging.LogManager;
 
 public class Core {
 	public static LogManager logManager ;
+	public static PeerDiscovery peerDiscovery;
 	public static PeerDataHandler pdh;
 	public static ConfigInit config;
 	public static ConnectionIDHander cIDHandle;
@@ -19,7 +21,7 @@ public class Core {
     public static void main(String[] args) {
     	UIDHander = new PeerUIDHander();
     	logManager = new LogManager();
-    	FileTransforms.FileToPices("/proc/cmdline",40);
+    	logManager.log(Core.class.getName(),"Pieces:"+FileTransforms.FileToPices("/proc/cmdline",40));
     	logManager.log(Core.class.getName(), "Welcome to AnonFS version: "+Configuration.version+" core now alive!");
     	logManager.log(Core.class.getName(), "UID:"+UIDHander.getUIDString());
     	cIDHandle = new ConnectionIDHander();
@@ -32,6 +34,8 @@ public class Core {
     	pdh = new PeerDataHandler();
     	AutoConnector ac = new AutoConnector();
     	ac.start();
+    	peerDiscovery = new PeerDiscovery();
+    	peerDiscovery.start();
     	TCPServer tcpServer = new TCPServer(Configuration.DEFAULT_PORT);
     }
 }

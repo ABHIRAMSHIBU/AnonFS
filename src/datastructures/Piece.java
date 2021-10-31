fbd38a286546bc549ba70782b84151d5ab3205cf
Abhiram Shibu <abhiramshibu1998@gmail.com>
2021-10-31 09:31:43 +0530

    Piece: Added checksum and signature getter and setter

--- src/datastructures/Piece.java
+++ src/datastructures/Piece.java
@@ -3,6 +3,25 @@ package datastructures;
 
 public class Piece {
 	public byte body[];
-	public long seqnum;
 	public byte signature[];
+	public byte checksum[];
+	
+	public byte[] getChecksum() {
+		return checksum;
+	}
+	public void setChecksum(byte[] checksum) {
+		this.checksum = checksum;
+	}
+	public byte[] getSignature() {
+		return signature;
+	}
+	public void setSignature(byte[] signature) {
+		this.signature = signature;
+	}
+	public byte[] getBody() {
+		return body;
+	}
+	public void setBody(byte[] body) {
+		this.body = body;
+	}
 }

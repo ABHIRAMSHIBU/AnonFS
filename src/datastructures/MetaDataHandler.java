8fc27276cfe069e5e684f5f451f46a634d8ab779
Abhiram Shibu <abhiramshibu1998@gmail.com>
2021-12-19 23:43:39 +0530

    MetaDataHander: Metadata serializer added

--- src/datastructures/MetaDataHandler.java
+++ src/datastructures/MetaDataHandler.java
@@ -2,6 +2,8 @@ package datastructures;
 
 import java.util.HashMap;
 
+import algorithm.ByteArrayTransforms;
+
 public class MetaDataHandler {
 	HashMap<Long, byte[]> table = new HashMap<Long, byte[]>();
 	
@@ -31,4 +33,30 @@ public class MetaDataHandler {
 		return table.size();
 	}
 	
+	public String toString() {
+		String metadataString = "";
+		for(long i=0; i<table.size(); i++) {
+			byte [] checksum = table.get(i);
+			metadataString += ByteArrayTransforms.toHexString(checksum) + new String("\n"); 
+		}
+		return metadataString;
+	}
+	
+	public void fromString(String input) throws Exception {
+		if(table.size()!=0) {
+			throw(new Exception("Metadata Hander is not Empty!"));
+		}
+		long entry = 0;
+		int oldindex = -1;
+		int index = input.indexOf("\n",oldindex+1);
+		while(index!=-1) {
+			// Read all char except \n
+			String line = input.substring(oldindex+1,index);
+			byte [] checksum = ByteArrayTransforms.HexStringToBytes(line);
+			table.put(entry, checksum);
+			entry++;
+			oldindex = index;
+			index = input.indexOf("\n",oldindex+1);
+		}
+	}
 }

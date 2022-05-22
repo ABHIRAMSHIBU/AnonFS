89f74feb7557d35ccba12dd43cea4dff3d85c14a
Abhiram Shibu <abhiramshibu1998@gmail.com>
2022-05-22 08:32:13 +0530

    AnonFS:Docker: Update creation, starting and attach script

--- docker/container/attach.sh
+++ docker/container/attach.sh
@@ -1,5 +1,17 @@
-printf "Enter nodeid 2-4:"
-read nodeid
-echo Attaching to node anonfs_node$nodeid. To detach use Ctrl+p Ctrl+q
-sudo docker attach anonfs_node$nodeid
-
+MAXPEERS=4
+MINPEERS=2
+if [ $# -ne 1 ]
+then
+	echo "Not enough or too many arguments"
+	echo "./<script>.sh node_id"
+	exit -1
+fi
+if [ $1 -lt $MINPEERS ] || [ $1 -gt $MAXPEERS ]
+then
+	echo "Arguemnt out of range"
+	echo "It should be between $MINPEERS-$MAXPEERS"
+else
+	nodeid=$1
+	echo Attaching to node anonfs_node$nodeid. To detach use Ctrl+p Ctrl+q
+	sudo docker attach anonfs_node$nodeid
+fi
--- docker/container/start.sh
+++ docker/container/start.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+./check_http_server.sh
+if [ 0 -ne $? ]
+then
+	echo "HTTP Error cannot proceed"
+	exit -1
+fi
 echo "Starting anonfs_node2"
 sudo docker container start anonfs_node2
 echo "Starting anonfs_node3"
